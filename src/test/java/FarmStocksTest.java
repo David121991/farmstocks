86253d13e332876659caf983534ec881e9b9567f
jlashgar <jlashgar@students.kennesaw.edu>
2019-07-15 12:14:42 -0400

    first commit

---
+++ src/test/java/FarmStocksTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FarmStocksTest {
+
+    private FarmStocksDB db;
+
+    @BeforeEach
+    void setUp() {
+        db = new FarmStocksDB("DB/FarmStocks.xlsx");
+    }
+
+    @AfterEach
+    void tearDown() {
+        db = null;
+    }
+
+    @Test
+    void testSupplies() {
+        int numSupplies = 15;
+        assertEquals(numSupplies, db.getNumSupplies());
+        assertEquals(numSupplies, db.getSupplies().size());
+    }
+
+    @Test
+    void testSupplyQuantity() {
+        int quantity = 50;
+        assertEquals(quantity, db.getSupplyByName("Grain").getQuantity());
+    }
+
+    @Test
+    void testSupplyPrice() {
+        double price = 100.00;
+        assertEquals(price, db.getSupplyByName("Tractors").getPrice());
+    }
+
+    @Test
+    void testNearestLocation() {
+        String city = "Buford";
+        assertEquals(city, db.getSupplyByName("Livestock").getNearestLocation());
+    }
+
+    @Test
+    void testBuy() {
+        Supply combine = db.getSupplyByName("Combine");
+        assertEquals(0, combine.getQuantity());
+        combine.buy(1);
+        assertEquals(1, combine.getQuantity());
+    }
+
+    @Test
+    void testBuyNegative() {
+        Supply combine = db.getSupplyByName("Combine");
+        assertEquals(0, combine.getQuantity());
+        assertThrows(IllegalArgumentException.class, () -> combine.buy(-1));
+    }
+
+    @Test
+    void testBuyZero() {
+        Supply combine = db.getSupplyByName("Combine");
+        assertEquals(0, combine.getQuantity());
+        combine.buy(0);
+        assertEquals(0, combine.getQuantity());
+    }
+
+    @Test
+    void testBuyMultiple() {
+        Supply combine = db.getSupplyByName("Combine");
+        assertEquals(0, combine.getQuantity());
+        combine.buy(2);
+        assertEquals(2, combine.getQuantity());
+    }
+}
